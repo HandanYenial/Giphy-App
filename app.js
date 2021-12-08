4a2491569c53bf9dd94574a49ae3554fd64ed53f
harun <handan.yenial@gmail.com>
2021-12-08 11:01:39 -0500

    new commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const $gifArea = $("#gif-area"); // const gifArea=document.getElementById("#gif-area");
+const $searchInput = $("#search"); //const searchInput=document.getElementById("#search");
+
+/* use ajax result to add a gif */
+
+function addGif(res) {
+  let numResults = res.data.length;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+    let $newGif = $("<img>", {
+      src: res.data[randomIdx].images.original.url,
+      class: "w-100"
+    });
+    $newCol.append($newGif);
+    $gifArea.append($newCol);
+  }
+}
+
+/* handle form submission: clear search box & make ajax call */
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "Yy5ZVbBMmyKWzga449o2e4xm7wHP3o1T"
+    }
+  });
+  addGif(response.data);
+});
+
+/* remove gif */
+
+$("#remove").on("click", function() {
+  $gifArea.empty();
+});
